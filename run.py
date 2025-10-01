daddca9252d8a1e75a89f8f5238b5dadb331508b
DavidKart <david@kartte.net>
2025-10-01 10:23:11 +0200

    added pixel size and ctffplotter

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import os
 import glob
 import re
 import numpy as np
+import subprocess
 
 startingDirectory = os.path.realpath(os.path.dirname(__file__))
 os.chdir(startingDirectory)
@@ -11,10 +12,31 @@ from scripts import predictForAllTomos
 
 
 #_______________________initialize input parameters__________________
-reconstructHalfMaps = True #Creation of tomograms and tomogram half maps from raw data
-trainCryoCARE = True #cryoCARE train
+reconstructHalfMaps = True #True #Creation of tomograms and tomogram half maps from raw data
+trainCryoCARE = True #False #cryoCARE train
 predictCryoCARE = True #cryoCRAE predict
 
+# general
+apix = "read" #read from raw .tif files if set to "read". If that information is incorrect, just input give number.
+mdocPreprocessing = "mdocs/*.mdoc" #"*.mdoc" takes only data in the present directory. Otherwise just give one mdoc or specifiy multiple, for example "TS[2-4].mdoc"
+mdocryoCARETrain = "mdocsTrain/*.mdoc" #choose a small subset of representative tomos
+mdocryoCAREPredict = "mdocs/*.mdoc"
+ending="tiff" #tif, tiff, mrc, eer
+gain="" #input name of gain (in .mrc format! if ".dm4" convert with: 'dm2mrc input_ref.dm4 output_ref.mrc'
+
+#MotionCor3
+flipy = 2 #2 corresponds to y-flip. Check 'MotionCor3 --help' for more information. Use 0 for no flip.
+mcBinning = 2 #binning in MotionCor3. Probably makes things a bit faster. 1 means no binning!
+gpus = "-Gpu 0 1" #MotionCore3. For AreTomo there does not seem to be an option yet for multiple gpu usage.
+
+#areTomo2
+binning = 4 #areTomo2 binning. Overall binning should be 'mcBinning*binning'.
+locAlign = "-Patch 4 5"
+applyTiltAxisCorrection = 1
+
+
+
+#expert options__________
 divByTilt = False
 
 #executables
@@ -22,26 +44,14 @@ motionCor = "MotionCor3"
 areTomo = "AreTomo2"
 imodNewStack = "newstack"
 
-#general
-mdocPreprocessing = "mdocs/*.mdoc" #"*.mdoc" takes only data in the present directory. Otherwise just give one mdoc or specifiy multiple, for example "TS[2-4].mdoc"
-mdocryoCARETrain = "mdocs_train/*.mdoc" #choose a small subset of representative tomos
-mdocryoCAREPredict = "mdocs/*.mdoc"
-ending="eer" #tif, tiff, mrc, eer
-gain="../20220406_175020_EER_GainReference.gain" #input name of gain (in .mrc format! if ".dm4" convert with: 'dm2mrc input_ref.dm4 output_ref.mrc'
-
-#MotionCor3
-flipy = 0 #2 corresponds to y-flip. Check 'MotionCor3 --help' for more information. Use 0 for no flip.
-mcBinning = 2 #binning in MotionCor3. Probably makes things a bit faster. 1 means no binning!
-gpus = "-Gpu 0 1" #MotionCore3. For AreTomo there does not seem to be an option yet for multiple gpu usage.
+#CTF correction - correct CTF for aligned stacks with ctfplotter!
+include_CTFcorrection = False
+expectedDefocus = "read" #read automatically from mdoc file
+TiltAxisRotation = "read" #read automatically from mdoc file
 
 #only applicable if ending == "eer"
 eerSampling = "1"
 fmIntFile = "fmintFile"
-
-#areTomo2
-binning = 4 #areTomo2 binning. Overall binning should be 'mcBinning*binning'.
-locAlign = "-Patch 4 5"
-
 
 #________________________run_________________________________________
 #cryoCARE #####
@@ -52,7 +62,11 @@ if divByTilt and (len(locAlign) > 1):
 	print("align local patches only when averaging frames")
 
 
-
+if applyTiltAxisCorrection == 1:
+    applyTiltAxisCorrection = " -TiltCor 1"
+else:
+    applyTiltAxisCorrection = " -TiltCor 0"
+    
 def splitAlnFile(alnFileName, evenalnFileName, oddalnFileName):
 	print("apply split of aln file")
 	with open(alnFileName, "r") as readfile:
@@ -151,7 +165,7 @@ def runMotionCorr(fileList):
 	
 	return averagedList
 
-def rec():
+def rec(apix, expectedDefocus, TiltAxisRotation):
 	#1. read mdoc files
 	mdocFiles = glob.glob(os.path.join(mdocPreprocessing))
 	if len(mdocFiles) == 0:
@@ -165,8 +179,18 @@ def rec():
 	 
 		angles=[]
 		names=[]
+		current_tiltAxisAngle = 0
+		current_expectedDefocus = 0
 		with open(files, "r") as readfile:
 			for line in readfile:
+				if include_CTFcorrection:
+					matchTAA = re.search(r"Tilt axis angle\s*=\s*([0-9.]+)", line)
+					matchTDE = re.search(r"TargetDefocus\s*=\s*(-?\d+)", line)
+					if matchTAA:
+						current_tiltAxisAngle = float(matchTAA.group(1))
+					if matchTDE:
+						current_expectedDefocus = float(matchTDE.group(1)) * -1000
+						
 				if "TiltAngle" in line:
 					angle=line.strip()
 					angle= re.findall(r"[-+]?\d*\.\d+|\d+", angle)
@@ -175,6 +199,24 @@ def rec():
 					fileName = line.strip()
 					fileName = fileName.split("\\")[-1]
 					names.append(fileName)
+
+
+		# fill variables correctly
+		if apix == "read":
+			cmd = f"header {names[0]} | grep 'Pixel spacing' | awk '{{print $4}}'"
+			apix = subprocess.check_output(cmd, shell=True, text=True).strip()
+		apix = float(apix)
+   
+		if include_CTFcorrection:
+			if expectedDefocus != "read":
+				current_expectedDefocus = expectedDefocus
+			if TiltAxisRotation != "read":
+				current_tiltAxisAngle = TiltAxisRotation
+			#check if all is fine
+			if isinstance(apix, str) or isinstance(current_expectedDefocus, str) or isinstance(current_tiltAxisAngle, str):
+				print("ERROR for given apix, tilt axis angle or expected defocus. Cannot apply CTF correction. skip tilt series.")
+
+
 
 		#2. output dirs
 		#make output directory
@@ -189,8 +231,8 @@ def rec():
 		evenPath = os.path.join(startingDirectory, evenPrefix)
 
 		print("made dirs")
-
-
+   		
+     
 		#3. produce full MotionCorrected Tomogram!
 		runMotionCorr(names)
 
@@ -229,11 +271,28 @@ def rec():
 		stackOutFileName = tomoName + "_stacked.mrc"
 		os.system(imodNewStack+ " " + stackList + " " + stackOutFileName)
 
+		if include_CTFcorrection:
+			print("PERFOM CTF ESTIMATION OF TILT SERIES")
+			print("pixel size: " + str(apix) + "\ntilt axis angle: "+ str(current_tiltAxisAngle)+ "\nexpected defocus: " + str(current_expectedDefocus))
+			outDefFile = "defocusFile.txt"
+			ctf_stackOutFileName = "ctfCorrected_" + stackOutFileName
+			
+			os.system("ctfplotter -inp " + stackOutFileName + " -vo 300 -cs 2.7 -pi " + str(apix) + " -an "+ rawtlt + " -exp " + str(current_expectedDefocus) + " -defF " + str(outDefFile) + " -aA " + str(current_tiltAxisAngle)+ " -au 1,1 -use -ba 2 -va -fit 4 -sa -sA")
+			print("PERFOM CTF CORRECTION OF TILT SERIES")
+			os.system("ctfphaseflip -inp " + stackOutFileName + " -o " + ctf_stackOutFileName +" -an "+ rawtlt + " -defF " + str(outDefFile) + " -gpu 0 -defT 1 -iW 1 -pi " + str(apix) + " -vo 300 -cs 2.7 -am 0.07 -ax " + str(current_tiltAxisAngle))
+			stackOutFileName = ctf_stackOutFileName
+
+			os.system("cp " + outDefFile + " " + evenPath)
+			os.system("cp " + outDefFile + " " + oddPath)
+  
 		#6. run AreTomo for full tomogram
+		apixAreTomo = ""
+		if not isinstance(apix, str):
+			apixAreTomo = " -PixSize " + str(mcBinning*apix)
 		bin = "-OutBin " + str(binning) 
 		alignedName=tomoName+"_rec_tomo.mrc"
-		os.system(areTomo+" -InMrc "+ stackOutFileName + " -OutMrc " + alignedName + " -VolZ 1200 " + bin + " -AngFile " + rawtlt + " -TiltCor 1 -DarkTol 0.5 -OutXf 1 -Wbp 1 -FlipVol 1 " + locAlign + " >" + tomoName + "_logFile.txt")
-
+		os.system(areTomo+" -InMrc "+ stackOutFileName + " -OutMrc " + alignedName + apixAreTomo + " -VolZ 1200 " + bin + " -AngFile " + rawtlt + applyTiltAxisCorrection + " -DarkTol 0.5 -OutXf 1 -Wbp 1 -FlipVol 1 " + locAlign + " >" + tomoName + "_logFile.txt")
+		ctf_stackOutFileNameFull = stackOutFileName
 		#cp result
 		alnFileName = stackOutFileName[:-4] + ".aln"
   
@@ -259,8 +318,19 @@ def rec():
 			stackOutFileName = "even_" + tomoName + "_stacked.mrc"
 			os.system(imodNewStack + " " + stackList_even + " " + stackOutFileName)
 		
+			if include_CTFcorrection:
+				print("PERFOM CTF CORRECTION OF EVEN TILT SERIES")
+				# print("pixel size: " + str(apix) + "\ntilt axis angle: "+ str(current_tiltAxisAngle)+ "\nexpcected defocus: " + str(current_expectedDefocus))
+				# outDefFile = "defocusFile.txt"
+				ctf_stackOutFileName = "ctfCorrected_" + stackOutFileName
+				
+				# os.system("ctfplotter -inp " + stackOutFileName + " -vo 300 -cs 2.7 -pi " + str(apix) + " -an "+ rawtlt + " -exp " + str(current_expectedDefocus) + " -defF " + str(outDefFile) + " -aA " + str(current_tiltAxisAngle)+ " -au 1,1 -use -ba 2 -va -fit 4 -sa -sA")
+				os.system("ctfphaseflip -inp " + stackOutFileName + " -o " + ctf_stackOutFileName +" -an "+ rawtlt + " -defF " + str(outDefFile) + " -gpu 0 -defT 1 -iW 1 -pi " + str(apix) + " -vo 300 -cs 2.7 -am 0.07 -ax " + str(current_tiltAxisAngle))
+				stackedFileEVN_name = ctf_stackOutFileName
+
+  
 		# os.system(areTomo+" -InMrc "+ stackOutFileName + " -OutMrc even_" + alignedName + " -VolZ 1200 " + bin + "  -AngFile " + rawtlt + " -AlnFile " + alnFileName +" -TiltCor 1 -DarkTol 0.01 -OutXf 1 -Wbp 1 -FlipVol 1" + locAlign + ">" + tomoName + "_even_logFile.txt")
-		os.system(areTomo+" -InMrc "+ stackedFileEVN_name + " -OutMrc even_" + alignedName + " -VolZ 1200 " + bin + " -AlnFile " + alnFileNameEVN +" -TiltCor 1 -DarkTol 0.5 -OutXf 1 -Wbp 1 -FlipVol 1 " + locAlign + " >" + tomoName + "_even_logFile.txt")
+		os.system(areTomo+" -InMrc "+ stackedFileEVN_name + " -OutMrc even_" + alignedName + apixAreTomo + " -VolZ 1200 " + bin + " -AlnFile " + alnFileNameEVN + applyTiltAxisCorrection + " -DarkTol 0.5 -OutXf 1 -Wbp 1 -FlipVol 1 " + locAlign + " >" + tomoName + "_even_logFile.txt")
 
 
 		os.chdir(oddPath)
@@ -269,8 +339,17 @@ def rec():
 			stackOutFileName = "odd_" + tomoName + "_stacked.mrc"
 			os.system(imodNewStack + " " + stackList_odd + " " + stackOutFileName)
    
-		# os.system(areTomo+" -InMrc "+ stackOutFileName + " -OutMrc odd_" + alignedName + " -VolZ 1200 " + bin + "  -AngFile " + rawtlt + " -AlnFile " + alnFileName +" -TiltCor 1 -DarkTol 0.01 -OutXf 1 -Wbp 1 -FlipVol 1" + locAlign + ">" + tomoName + "_odd_logFile.txt")
-		os.system(areTomo+" -InMrc "+ stackedFileNameODD_name + " -OutMrc odd_" + alignedName + " -VolZ 1200 " + bin + " -AlnFile " + alnFileNameODD +" -TiltCor 1 -DarkTol 0.5 -OutXf 1 -Wbp 1 -FlipVol 1 " + locAlign + " >" + tomoName + "_odd_logFile.txt")
+			if include_CTFcorrection:
+				print("PERFOM CTF CORRECTION OF ODD TILT SERIES")
+				# print("pixel size: " + str(apix) + "\ntilt axis angle: "+ str(current_tiltAxisAngle)+ "\nexpcected defocus: " + str(current_expectedDefocus))
+				# outDefFile = "defocusFile.txt"
+				ctf_stackOutFileName = "ctfCorrected_" + stackOutFileName
+				
+				# os.system("ctfplotter -inp " + stackOutFileName + " -vo 300 -cs 2.7 -pi " + str(apix) + " -an "+ rawtlt + " -exp " + str(current_expectedDefocus) + " -defF " + str(outDefFile) + " -aA " + str(current_tiltAxisAngle)+ " -au 1,1 -use -ba 2 -va -fit 4 -sa -sA")
+				os.system("ctfphaseflip -inp " + stackOutFileName + " -o " + ctf_stackOutFileName +" -an "+ rawtlt + " -defF " + str(outDefFile) + " -gpu 0 -defT 1 -iW 1 -pi " + str(apix) + " -vo 300 -cs 2.7 -am 0.07 -ax " + str(current_tiltAxisAngle))
+				stackedFileNameODD_name = ctf_stackOutFileName
+    
+		os.system(areTomo+" -InMrc "+ stackedFileNameODD_name + " -OutMrc odd_" + alignedName + apixAreTomo + " -VolZ 1200 " + bin + " -AlnFile " + alnFileNameODD + applyTiltAxisCorrection + " -DarkTol 0.5 -OutXf 1 -Wbp 1 -FlipVol 1 " + locAlign + " >" + tomoName + "_odd_logFile.txt")
 
 		#create rawTomos dir and move half maps in preparation for cryoCARE
 		if not os.path.exists(cryoCAREpath) or not os.path.isdir(cryoCAREpath):
@@ -283,14 +362,18 @@ def rec():
 		#Establish some order
 		os.chdir(startingDirectory)
 		os.system("mkdir newTomo")
+		os.system("mv " + ctf_stackOutFileNameFull + " newTomo")
+		os.system("mv " + alnFileName + " newTomo")
 		os.system("mv " + tomoName + "* " + "newTomo")
+		if include_CTFcorrection:
+			os.system("mv " + outDefFile + " newTomo")
 		os.system("mv newTomo rec_" + tomoName)
 
 
 
 def main():
 	if reconstructHalfMaps:
-		rec()
+		rec(apix, expectedDefocus, TiltAxisRotation)
 	
 	tomosToUseTrain = []
 	tomosToUsePredict = []
